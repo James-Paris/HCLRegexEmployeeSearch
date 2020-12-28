413bd6dbc4f807ffface25712182c1bb51264895
James <James.Paris@Hcl.com>
2020-12-28 16:23:37 -0600

    Created an employee

--- src/Employees.java
+++ src/Employees.java
@@ -1,4 +1,31 @@
 
 public class Employees {
 
+	String[] emailIds;
+	
+	int nextIndex = 0;
+	int numEmployees = 0;
+	
+	
+	public Employees(int numEmployees) {
+		this.numEmployees = numEmployees;
+		this.emailIds = new String[numEmployees];
+	}
+	
+	public void addEmployee(String emailId) {
+		this.emailIds[this.nextIndex] = emailId;
+		this.nextIndex++;
+		if(this.nextIndex >= this.numEmployees) {
+			this.nextIndex = 0;
+		}
+	}
+	
+	public void printEmployees() {
+		for(int j=0; j<this.emailIds.length; j++) {
+			if(null == this.emailIds[j])
+				continue;
+			
+			System.out.println((j+1) + ": " + this.emailIds[j]);
+		}
+	}
 }
