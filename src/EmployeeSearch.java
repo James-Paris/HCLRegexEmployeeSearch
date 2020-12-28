ac5bed1e181d4d296fadca6fd24e70f975aca763
James <James.Paris@Hcl.com>
2020-12-28 16:24:03 -0600

    Implemented Employees and printed them out

--- src/EmployeeSearch.java
+++ src/EmployeeSearch.java
@@ -1,4 +1,17 @@
 
 public class EmployeeSearch {
 
+	public static void main(String[] args) {
+		
+		Employees workers = new Employees(5);
+		
+		workers.addEmployee("joe");
+		workers.addEmployee("me@mail.com");
+		workers.addEmployee("joey@gmail.com");
+		workers.addEmployee("terry@hcl.com");
+		workers.addEmployee("susan");
+		
+		workers.printEmployees();
+	}
+	
 }
